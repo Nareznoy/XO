ddb249daf54303a9e0b828649431a64bcef65caf
Денис Северин <deniseverin@mail.ru>
2020-05-09 14:08:40 +0300

    Добавьте файлы проекта.

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button0_0 = new System.Windows.Forms.Button();
+            this.button0_1 = new System.Windows.Forms.Button();
+            this.button0_2 = new System.Windows.Forms.Button();
+            this.button1_0 = new System.Windows.Forms.Button();
+            this.button1_1 = new System.Windows.Forms.Button();
+            this.button1_2 = new System.Windows.Forms.Button();
+            this.button2_0 = new System.Windows.Forms.Button();
+            this.button2_1 = new System.Windows.Forms.Button();
+            this.button2_2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(220, 136);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.button0_0, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button0_1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button0_2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1_0, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1_1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1_2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2_0, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2_1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2_2, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(357, 238);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(241, 190);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button0_0
+            // 
+            this.button0_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0_0.Location = new System.Drawing.Point(3, 3);
+            this.button0_0.Name = "button0_0";
+            this.button0_0.Size = new System.Drawing.Size(74, 57);
+            this.button0_0.TabIndex = 0;
+            this.button0_0.UseVisualStyleBackColor = true;
+            this.button0_0.Click += new System.EventHandler(this.button0_0_Click);
+            // 
+            // button0_1
+            // 
+            this.button0_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0_1.Location = new System.Drawing.Point(83, 3);
+            this.button0_1.Name = "button0_1";
+            this.button0_1.Size = new System.Drawing.Size(74, 57);
+            this.button0_1.TabIndex = 1;
+            this.button0_1.UseVisualStyleBackColor = true;
+            this.button0_1.Click += new System.EventHandler(this.button0_1_Click);
+            // 
+            // button0_2
+            // 
+            this.button0_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0_2.Location = new System.Drawing.Point(163, 3);
+            this.button0_2.Name = "button0_2";
+            this.button0_2.Size = new System.Drawing.Size(75, 57);
+            this.button0_2.TabIndex = 2;
+            this.button0_2.UseVisualStyleBackColor = true;
+            this.button0_2.Click += new System.EventHandler(this.button0_2_Click);
+            // 
+            // button1_0
+            // 
+            this.button1_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1_0.Location = new System.Drawing.Point(3, 66);
+            this.button1_0.Name = "button1_0";
+            this.button1_0.Size = new System.Drawing.Size(74, 57);
+            this.button1_0.TabIndex = 3;
+            this.button1_0.UseVisualStyleBackColor = true;
+            this.button1_0.Click += new System.EventHandler(this.button1_0_Click);
+            // 
+            // button1_1
+            // 
+            this.button1_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1_1.Location = new System.Drawing.Point(83, 66);
+            this.button1_1.Name = "button1_1";
+            this.button1_1.Size = new System.Drawing.Size(74, 57);
+            this.button1_1.TabIndex = 4;
+            this.button1_1.UseVisualStyleBackColor = true;
+            this.button1_1.Click += new System.EventHandler(this.button1_1_Click);
+            // 
+            // button1_2
+            // 
+            this.button1_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1_2.Location = new System.Drawing.Point(163, 66);
+            this.button1_2.Name = "button1_2";
+            this.button1_2.Size = new System.Drawing.Size(75, 57);
+            this.button1_2.TabIndex = 5;
+            this.button1_2.UseVisualStyleBackColor = true;
+            this.button1_2.Click += new System.EventHandler(this.button1_2_Click);
+            // 
+            // button2_0
+            // 
+            this.button2_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2_0.Location = new System.Drawing.Point(3, 129);
+            this.button2_0.Name = "button2_0";
+            this.button2_0.Size = new System.Drawing.Size(74, 58);
+            this.button2_0.TabIndex = 6;
+            this.button2_0.UseVisualStyleBackColor = true;
+            this.button2_0.Click += new System.EventHandler(this.button2_0_Click);
+            // 
+            // button2_1
+            // 
+            this.button2_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2_1.Location = new System.Drawing.Point(83, 129);
+            this.button2_1.Name = "button2_1";
+            this.button2_1.Size = new System.Drawing.Size(74, 58);
+            this.button2_1.TabIndex = 7;
+            this.button2_1.UseVisualStyleBackColor = true;
+            this.button2_1.Click += new System.EventHandler(this.button2_1_Click);
+            // 
+            // button2_2
+            // 
+            this.button2_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2_2.Location = new System.Drawing.Point(163, 129);
+            this.button2_2.Name = "button2_2";
+            this.button2_2.Size = new System.Drawing.Size(75, 58);
+            this.button2_2.TabIndex = 8;
+            this.button2_2.UseVisualStyleBackColor = true;
+            this.button2_2.Click += new System.EventHandler(this.button2_2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button0_0;
+        private System.Windows.Forms.Button button0_1;
+        private System.Windows.Forms.Button button0_2;
+        private System.Windows.Forms.Button button1_0;
+        private System.Windows.Forms.Button button1_1;
+        private System.Windows.Forms.Button button1_2;
+        private System.Windows.Forms.Button button2_0;
+        private System.Windows.Forms.Button button2_1;
+        private System.Windows.Forms.Button button2_2;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public int[,] mass = new int[3, 3];
+        private bool haveWinner = false;
+        private bool player = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            int i = int.Parse(b.Name[6].ToString());
+            int j = int.Parse(b.Name[8].ToString());
+
+            if (mass[i, j] == 0)
+            {
+                changeButton(b, i, j, player);
+                if ((checkBox1.Checked) && (!haveWinner))
+                {
+                    getPoint(ref i, ref j);
+                    changeButton(b, i, j, false);
+                }
+                else
+                {
+                    player = !player;
+                }
+            }
+        }
+
+        private void changeButton(Button b, int i, int j, bool player)
+        {
+            b.Text = (player ? "X" : "O");
+            //либо так
+            //b.Image = (player ? img : img2);
+            b.Update();
+            mass[i, j] = (player ? 1 : 2);
+            checkWinner(player);
+        }
+        private void getPoint(ref int i, ref int j)
+        {
+            Random rand = new Random();
+            while (mass[i, j] != 0)
+            {
+                i = rand.Next(0, 3);
+                j = rand.Next(0, 3);
+            }
+        }
+
+        private void checkWinner(bool player)
+        {
+            int val = (player ? 1 : 2);
+            bool[] done = new bool[9];
+            for (int i = 0; i < done.Length; i++) done[i] = true;
+            for (int i = 0; i < 3; i++)
+            {
+                if (mass[i, i] != val) done[6] = false;
+                if (mass[i, 2 - i] != val) done[7] = false;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (mass[i, j] != val)
+                    {
+                        done[i] = false;
+                    }
+                    if (mass[j, i] != val) done[i + 3] = false;
+                    if (mass[i, j] == 0) done[8] = false;
+                }
+            }
+            for (int i = 0; i < done.Length; i++)
+            {
+                if (done[i] == true)
+                {
+                    haveWinner = true;
+                    for (int k = 0; k < 3; k++)
+                        for (int j = 0; j < 3; j++)
+                        {
+                            (this.Controls[string.Format("button{0}_{1}", k, j)] as Button).Enabled = false;
+                        }
+                    if (i == 8)
+                    {
+                        MessageBox.Show("Ничья!!!");
+                    }
+                    if (player)
+                    {
+                        if (checkBox1.Checked)
+                            MessageBox.Show("вы выиграли!!!");
+                        else MessageBox.Show("игрок X выиграл!!!");
+                    }
+                    else
+                    {
+                        if (checkBox1.Checked)
+                            MessageBox.Show("вы проиграли!!!");
+                        else MessageBox.Show("игрок O выиграл!!!");
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    Button bt = new Button();
+                    bt.Location = new System.Drawing.Point(30 * i + 10 * (i + 1), 30 * j + 10 * (j + 1));
+                    bt.Name = String.Format("button{0}_{1}", i, j);
+                    bt.Size = new System.Drawing.Size(30, 30);
+                    bt.Click += new System.EventHandler(this.button_Click);
+                    this.Controls.Add(bt);
+                }
+
+        }
+
+        private Button getButton(int i, int j)
+        {
+            if (i == 0 && j == 0)
+                return button0_0;
+            else if (i == 0 && j == 1)
+                return button0_1;
+            else if (i == 0 && j == 2)
+                return button0_2;
+            else if (i == 1 && j == 0)
+                return button1_0;
+            else if (i == 1 && j == 1)
+                return button1_1;
+            else if (i == 1 && j == 2)
+                return button1_2;
+            else if (i == 2 && j == 0)
+                return button2_0;
+            else if (i == 2 && j == 1)
+                return button2_1;
+            else if (i == 2 && j == 2)
+                return button2_2;
+            return null;
+        }
+
+        private void clickedButton(Button button, int i, int j)
+        {
+            if (mass[i, j] == 0)
+            {
+                changeButton(button, i, j, player);
+                if ((checkBox1.Checked) && (!haveWinner))
+                {
+                    getPoint(ref i, ref j);
+                    button = getButton(i, j);
+                    changeButton(button, i, j, false);
+                }
+                else
+                {
+                    player = !player;
+                }
+            }
+        }
+
+        private void button0_0_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 0, 0);
+        }
+
+        private void button0_1_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 0, 1);
+        }
+
+        private void button0_2_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 0, 2);
+        }
+
+        private void button1_0_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 1, 0);
+        }
+
+        private void button1_1_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 1, 1);
+        }
+
+        private void button1_2_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 1, 2);
+        }
+
+        private void button2_0_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 2, 0);
+        }
+
+        private void button2_1_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 2, 1);
+        }
+
+        private void button2_2_Click(object sender, EventArgs e)
+        {
+            clickedButton((Button)sender, 2, 2);
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    player = true;
+        //    haveWinner = false;
+        //    for (int i = 0; i < 3; i++)
+        //        for (int j = 0; j < 3; j++)
+        //        {
+        //            mass[i, j] = 0;
+        //            (this.Controls[string.Format("button{0}_{1}", i, j)] as Button).Enabled = true;
+        //            (this.Controls[string.Format("button{0}_{1}", i, j)] as Button).Image = null;
+        //        }
+        //}
+    }
+}
