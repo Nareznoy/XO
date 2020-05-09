0af7cf83c2ea388a43c967e2328bec81b9214fff
Денис Северин <deniseverin@mail.ru>
2020-05-09 19:05:10 +0300

    final

--- XO/Form1.cs
+++ XO/Form1.cs
@@ -1,25 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace XO
 {
     public partial class Form1 : Form
     {
-        private int[,] _fieldArray = new int[3, 3];
-        private bool _haveWinner = false;
+        private readonly bool[] _done = new bool[9];
+        private readonly int[,] _fieldArray = new int[3, 3];
         private bool _player = true;
-        private bool[] _done = new bool[9];
-
-        public bool HaveWinner { get { return _haveWinner; } }
-
 
 
         public Form1()
@@ -27,19 +15,44 @@ namespace XO
             InitializeComponent();
         }
 
+        public bool HaveWinner { get; private set; }
 
-        private void changeButton(Button b, int i, int j)
+
+        private void ClickedButton(Button button, int i, int j)
         {
-            b.Text = (_player ? "X" : "O");
-            b.Update();
-            _fieldArray[i, j] = (_player ? 1 : 2);
-            checkWinner();
+            if (_fieldArray[i, j] != 0) return;
+
+            ChangeButton(button, i, j);
+            if (checkBox1.Checked && !HaveWinner)
+            {
+                GetRandomPoint(ref i, ref j);
+                button = GetButton(i, j);
+
+                _player = false;
+                ChangeButton(button, i, j);
+                _player = true;
+            }
+            else
+            {
+                _player = !_player;
+            }
+
+            if (HaveWinner) ShowWinnerWindow();
         }
 
 
-        private void getRandomPoint(ref int i, ref int j)
+        private void ChangeButton(Button b, int i, int j)
         {
-            Random rand = new Random();
+            b.Text = _player ? "X" : "O";
+            b.Update();
+            _fieldArray[i, j] = _player ? 1 : 2;
+            CheckWinner();
+        }
+
+
+        private void GetRandomPoint(ref int i, ref int j)
+        {
+            var rand = new Random();
             while (_fieldArray[i, j] != 0)
             {
                 i = rand.Next(0, 3);
@@ -48,70 +61,77 @@ namespace XO
         }
 
 
-        private void checkWinner()
+        private Button GetButton(int i, int j)
         {
-            int val = (_player ? 1 : 2);
-            for (int i = 0; i < _done.Length; i++) 
+            return (Button)tableLayoutPanel1.Controls[$"button{i}_{j}"];
+        }
+
+
+        private void CheckWinner()
+        {
+            var val = _player ? 1 : 2;
+            for (var i = 0; i < _done.Length; i++)
                 _done[i] = true;
-            for (int i = 0; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
                 if (_fieldArray[i, i] != val) _done[6] = false;
                 if (_fieldArray[i, 2 - i] != val) _done[7] = false;
-                for (int j = 0; j < 3; j++)
+                for (var j = 0; j < 3; j++)
                 {
                     if (_fieldArray[i, j] != val) _done[i] = false;
                     if (_fieldArray[j, i] != val) _done[i + 3] = false;
                     if (_fieldArray[i, j] == 0) _done[8] = false;
                 }
             }
-            for (int i = 0; i < _done.Length; i++)
+
+            foreach (var win in _done)
+            {
+                if (!win) continue;
+
+                HaveWinner = true;
+                ChangeButtonsStatus(false);
+            }
+        }
+
+
+        private void ShowWinnerWindow()
+        {
+            for (var i = 0; i < _done.Length; i++)
             {
                 if (_done[i] == true)
                 {
-                    _haveWinner = true;
+                    if (i == 8)
+                    {
+                        var winnerForm = new WinnerForm(this, "Ничья!");
+                        winnerForm.ShowDialog();
+                        return;
+                    }
 
-                    changeButtonsStatus(false);
-                }
-            }
-        }
+                    if (!_player)
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            var winnerForm = new WinnerForm(this, "Вы выиграли!");
+                            winnerForm.ShowDialog();
+                            return;
+                        }
+                        else
+                        {
+                            var winnerForm = new WinnerForm(this, "Игрок X выиграл!");
+                            winnerForm.ShowDialog();
+                            return;
+                        }
+                    }
 
-
-        private void showWinnerWindow()
-        {
-            for (int i = 0; i < _done.Length; i++)
-            {
-                if (i == 8)
-                {
-                    WinnerForm winnerForm = new WinnerForm(this, "Ничья!");
-                    winnerForm.ShowDialog();
-                    return;
-                }
-                if (!_player)
-                {
                     if (checkBox1.Checked)
                     {
-                        WinnerForm winnerForm = new WinnerForm(this, "Вы выиграли!");
+                        var winnerForm = new WinnerForm(this, "Вы проиграли!");
                         winnerForm.ShowDialog();
                         return;
                     }
                     else
                     {
-                        WinnerForm winnerForm = new WinnerForm(this, "Игрок X выиграл!");
-                        winnerForm.ShowDialog();
-                        return;
-                    }
-                }
-                else
-                {
-                    if (checkBox1.Checked)
-                    {
-                        WinnerForm winnerForm = new WinnerForm(this, "Вы проиграли!");
-                        winnerForm.ShowDialog();
-                        return;
-                    }
-                    else
-                    {
-                        WinnerForm winnerForm = new WinnerForm(this, "Игрок O выиграл!");
+                        var winnerForm = new WinnerForm(this, "Игрок O выиграл!");
                         winnerForm.ShowDialog();
                         return;
                     }
@@ -120,85 +140,43 @@ namespace XO
         }
 
 
-        public void newGame()
+        private void ChangeButtonsStatus(bool status)
         {
-            clearField();
-            changeButtonsStatus(true);
-            _haveWinner = false;
+            for (var k = 0; k < 3; k++)
+            for (var j = 0; j < 3; j++)
+                ((Button)tableLayoutPanel1.Controls[$"button{k}_{j}"]).Enabled = status;
+        }
+
+
+        private void button0_0_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 0, 0); }
+        private void button0_1_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 0, 1); }
+        private void button0_2_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 0, 2); }
+        private void button1_0_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 1, 0); }
+        private void button1_1_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 1, 1); }
+        private void button1_2_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 1, 2); }
+        private void button2_0_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 2, 0); }
+        private void button2_1_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 2, 1); }
+        private void button2_2_Click(object sender, EventArgs e) { ClickedButton((Button)sender, 2, 2); }
+
+
+        public void NewGame()
+        {
+            ClearField();
+            ChangeButtonsStatus(true);
+            HaveWinner = false;
             _player = true;
         }
 
 
-        private void clickedButton(Button button, int i, int j)
+        private void ClearField()
         {
-            if (_fieldArray[i, j] == 0)
-            {
-                changeButton(button, i, j);
-                if ((checkBox1.Checked) && (!_haveWinner))
-                {
-                    getRandomPoint(ref i, ref j);
-                    button = getButton(i, j);
+            for (var i = 0; i < 3; i++)
+            for (var j = 0; j < 3; j++)
+                _fieldArray[i, j] = 0;
 
-                    _player = false;
-                    changeButton(button, i, j);
-                    _player = true;
-                }
-                else
-                {
-                    _player = !_player;
-                }
-                if (_haveWinner)
-                {
-                    showWinnerWindow();
-                }
-            }
+            for (var k = 0; k < 3; k++)
+            for (var j = 0; j < 3; j++)
+                ((Button) tableLayoutPanel1.Controls[$"button{k}_{j}"]).Text = "";
         }
-
-
-        private void button0_0_Click(object sender, EventArgs e) { clickedButton((Button)sender, 0, 0); }
-        private void button0_1_Click(object sender, EventArgs e) { clickedButton((Button)sender, 0, 1); }
-        private void button0_2_Click(object sender, EventArgs e) { clickedButton((Button)sender, 0, 2); }
-        private void button1_0_Click(object sender, EventArgs e) { clickedButton((Button)sender, 1, 0); }
-        private void button1_1_Click(object sender, EventArgs e) { clickedButton((Button)sender, 1, 1); }
-        private void button1_2_Click(object sender, EventArgs e) { clickedButton((Button)sender, 1, 2); }
-        private void button2_0_Click(object sender, EventArgs e) { clickedButton((Button)sender, 2, 0); }
-        private void button2_1_Click(object sender, EventArgs e) { clickedButton((Button)sender, 2, 1); }
-        private void button2_2_Click(object sender, EventArgs e) { clickedButton((Button)sender, 2, 2); }
-
-
-        private void changeButtonsStatus(bool status)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    (tableLayoutPanel1.Controls[string.Format("button{0}_{1}", k, j)] as Button).Enabled = status;
-                }
-            }
-        }
-
-
-        private void clearField()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    _fieldArray[i, j] = 0;
-                }
-            }
-
-            for (int k = 0; k < 3; k++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    (tableLayoutPanel1.Controls[string.Format("button{0}_{1}", k, j)] as Button).Text = "";
-                }
-            }
-        }
-
-
-        private Button getButton(int i, int j) => 
-            tableLayoutPanel1.Controls[string.Format("button{0}_{1}", i, j)] as Button;
     }
 }
--- XO/WinnerForm.cs
+++ XO/WinnerForm.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace XO
 {
     public partial class WinnerForm : Form
     {
-        private string _formText;
-        Form1 _opener;
+        private readonly string _formText;
+        private readonly Form1 _opener;
 
         public WinnerForm(Form1 opener, string formText)
         {
@@ -28,30 +21,25 @@ namespace XO
             label1.Text = _formText;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click_1(object sender, EventArgs e)
         {
-            this.Dispose();
-            _opener.newGame();
+            Dispose();
+            _opener.NewGame();
         }
 
         private void WinnerForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (_opener.HaveWinner)
+            if (!_opener.HaveWinner) return;
+
+            if (MessageBox.Show("Вы уверены, что хотите выйти?", "Предупреждение", MessageBoxButtons.YesNo) ==
+                DialogResult.Yes)
             {
-                if (MessageBox.Show("Вы уверены, что хотите выйти?", "Предупреждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                {
-                    this.Dispose();
-                    _opener.Dispose();
-                }
-                else
-                {
-                    e.Cancel = true;
-                }
+                Dispose();
+                _opener.Dispose();
+            }
+            else
+            {
+                e.Cancel = true;
             }
         }
     }
